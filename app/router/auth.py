fce99dfc7eb4bbf9accb7a957253b87784907b74
Isaac Oduh <ufuomaoduh@gmail.com>
2024-10-23 18:25:28 +0100

    implement login

--- app/router/auth.py
+++ app/router/auth.py
@@ -1,9 +1,11 @@
 from fastapi import Body, Depends, FastAPI, Response, status, HTTPException, Depends, APIRouter
+from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 from .. import schemas, utils, models
 from ..database import get_db
+from . import oauth2
 
-router = APIRouter(prefix="/auth", tags=["Users"])
+router = APIRouter(prefix="/auth", tags=["Authentication"])
 
 
 @router.post("", status_code=status.HTTP_201_CREATED, response_model=schemas.UserResponse)
@@ -16,3 +18,14 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     db.refresh(new_user)
     return new_user
 
+@router.post('/login')
+def login(user_card: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.email == user_card.username).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Invalid credentials")
+    if not utils.verify(user_card.password, user.password):
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Invalid credentials")
+
+    access_token = oauth2.create_access_token(data={'user_id': user.id})
+    return {"access_token": access_token, "token_type": "bearer"}
+    
--- app/schemas.py
+++ app/schemas.py
@@ -15,3 +15,14 @@ class UserResponse(BaseModel):
     id: int
 
     model_config = ConfigDict(arbitrary_types_allowed=True)
+
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    id: Optional[str] = None
