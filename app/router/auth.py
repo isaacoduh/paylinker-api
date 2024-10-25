7098475bed11b1dcfc1672fe5b26a065151c943a
Isaac Oduh <ufuomaoduh@gmail.com>
2024-10-25 15:04:39 +0100

    savepoint

--- app/router/auth.py
+++ app/router/auth.py
@@ -43,5 +43,5 @@ def login_json(
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Invalid credentials")
 
     access_token = oauth2.create_access_token(data={'user_id': user.id})
-    return {"access_token": access_token, "token_type": "bearer"}
+    return {"access_token": access_token, "token_type": "bearer", "user_id": user.id}
     
